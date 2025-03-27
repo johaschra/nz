84e87ccc50450b1781dbee4b3c6cecf4ec8742fc
Johanna Schramm <johanna.schramm@student.uibk.ac.at>
2025-03-27 15:54:04 +0100

    Variablen definieren lat, long zoom

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
-var map = L.map('map').setView([-42.114383, 171.327320], 13);
+let lat = -42.114383
+let lon = 171.327320
+let zoom = 11
+
+//l Karte initialisieren
+let map = L.map('map').setView([lat, lon], zoom);
+
+//Hintergrundkarte definierten
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([-42.114383, 171.327320]).addTo(map);
+
+// Marker zeichne 
+let marker = L.marker([-42.114383, 171.327320]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
